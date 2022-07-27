78583d7aa6daf0cbaca30f91a0bc84c95880adf8
Nikita Zaginey <nikitazaginey1@gmail.com>
2022-07-27 11:52:50 +0200

    add function for check and delete buttons

--- script.js
+++ script.js
@@ -4,32 +4,53 @@ const todoList = document.querySelector(".todo-list");
 
 //
 todoButton.addEventListener("click", addTodo);
-todoList.addEventListener("click")
+
+todoList.addEventListener("click", deleteCheck)
 
 //functions
 function addTodo(event){
+
     event.preventDefault();
     //Todo DiV
     const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");  
+    todoDiv.classList.add("todo");
+
     //create li
     const newTodo = document.createElement("li");
     newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
+
     //check button
     const completedButton = document.createElement("button");
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
+
     //delete button 
     const trashButton = document.createElement("button");
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
     trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
+
     //add list
     todoList.appendChild(todoDiv);
     todoInput.value = "";   
-    
-    
 }
+
+function deleteCheck(e){
+    const item = e.target;
+    //Delete
+    if(item.classList[0] === "trash-btn"){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+    
+    //Check
+     if(item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+
+}
+
