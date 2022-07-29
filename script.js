1df92f516587160383d5a28db2cef8adeac3d470
Nikita Zaginey <nikitazaginey1@gmail.com>
2022-07-29 22:13:14 +0200

    create new design

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const filterOption = document.querySelector(".filter-todo");
 document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
-filterOption.addEventListener("input", filterTodo);
+
 
 //functions
 function addTodo(event){
@@ -59,34 +59,6 @@ function deleteCheck(e) {
   }
 
 }
-
-
-function filterTodo(e) {
-
-    const todos = todoList.childNodes;
-    todos.forEach(function (todo) {
-      switch (e.target.value) {
-        case "all":
-          todo.style.display = "flex";
-          break;
-        case "completed":
-          if (todo.classList.contains("completed")) {
-            todo.style.display = "flex";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
-        case "uncompleted":
-          if (!todo.classList.contains("completed")) {
-            todo.style.display = "flex";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
-      }
-    });
-
-  }
   
   function saveLocalTodos(todo){
 
